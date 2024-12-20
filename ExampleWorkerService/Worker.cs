d85f568d9b573a8f8fe7f50b8eb9cc6be1bf06bc
Mahammad Isgandarov <mahammadi@risk.az>
2024-12-20 12:36:01 +0400

    Updating Log Information.

--- ExampleWorkerService/Worker.cs
+++ ExampleWorkerService/Worker.cs
@@ -15,7 +15,7 @@ namespace ExampleWorkerService
             {
                 if (_logger.IsEnabled(LogLevel.Information))
                 {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    _logger.LogInformation("Salamlar, Worker running at: {time}", DateTimeOffset.Now);
                 }
                 await Task.Delay(1000, stoppingToken);
             }
